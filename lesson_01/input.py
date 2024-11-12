ee2f8a8a99fb20d19cf631954e7e6c4de762689e
Esty Wolpo <stw2025@gmail.com>
2024-11-12 18:30:37 +0200

    adding files lesson_01

---
+++ lesson_01/input.py
@@ -0,0 +1,3 @@
+number_1 = input("enter the 1st number: ")
+number_2 = input("enter the 1st number: ")
+print (f"{number_1}  + {number_2} = {number_1 + number_2}")
---
+++ lesson_01/variables.py
@@ -0,0 +1,16 @@
+
+
+my_dict = {"age": 35, "name": "Alex", "first_name": 45}
+print(id(my_dict))
+print(type((1, 3)))
+
+my_dict = {"age": 35, "name": "Alex", "first_name": 45}
+print(type(my_dict))
+
+ids = {45, 67, 89, 45}
+print(type(ids))
+print(ids)
+
+is_true = True
+is_true = 8
+print(is_true)
