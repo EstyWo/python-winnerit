14ea412ac89127f505f1566eafbbc7a2d603cac7
EstyWo <stw2025@gmail.com>
2024-12-17 13:41:25 +0200

    adding headless flag

--- conftest.py
+++ conftest.py
@@ -1,10 +1,14 @@
 import pytest
 from selenium import webdriver
 from time import sleep
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture
 def browser():
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options
     driver = webdriver.Chrome()
     driver.maximize_window()
     yield driver
